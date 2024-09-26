bb6fbf9867b19f9dc865cd1808557a06a6f3411d
Ghaadyy <ghadyyi@gmail.com>
2024-09-26 14:51:56 +0300

    Setup the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,132 +1,198 @@
-import "./App.css";
-
 import {
   Menubar,
-  MenubarCheckboxItem,
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarRadioGroup,
-  MenubarRadioItem,
   MenubarSeparator,
   MenubarShortcut,
-  MenubarSub,
-  MenubarSubContent,
-  MenubarSubTrigger,
   MenubarTrigger,
 } from "@/components/ui/menubar";
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+// import Editor from "react-simple-code-editor";
+import { Input } from "@/components/ui/input";
+
+import { useState } from "react";
+
+import Editor from "@monaco-editor/react";
+
 function App() {
+  const [code, setCode] = useState<string>("");
+  const [urlSrc, setUrlSrc] = useState<string>(
+    "https://www.selenium.dev/documentation"
+  );
+
   return (
-    <>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        gap: 10,
+        padding: 10,
+      }}
+    >
       <Menubar>
         <MenubarMenu>
           <MenubarTrigger>File</MenubarTrigger>
           <MenubarContent>
             <MenubarItem>
-              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+              Open <MenubarShortcut>⌘O</MenubarShortcut>
             </MenubarItem>
-            <MenubarItem>
-              New Window <MenubarShortcut>⌘N</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem disabled>New Incognito Window</MenubarItem>
-            <MenubarSeparator />
-            <MenubarSub>
-              <MenubarSubTrigger>Share</MenubarSubTrigger>
-              <MenubarSubContent>
-                <MenubarItem>Email link</MenubarItem>
-                <MenubarItem>Messages</MenubarItem>
-                <MenubarItem>Notes</MenubarItem>
-              </MenubarSubContent>
-            </MenubarSub>
             <MenubarSeparator />
             <MenubarItem>
-              Print... <MenubarShortcut>⌘P</MenubarShortcut>
+              Save <MenubarShortcut>⌘S</MenubarShortcut>
             </MenubarItem>
           </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Edit</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem>
-              Undo <MenubarShortcut>⌘Z</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem>
-              Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarSub>
-              <MenubarSubTrigger>Find</MenubarSubTrigger>
-              <MenubarSubContent>
-                <MenubarItem>Search the web</MenubarItem>
-                <MenubarSeparator />
-                <MenubarItem>Find...</MenubarItem>
-                <MenubarItem>Find Next</MenubarItem>
-                <MenubarItem>Find Previous</MenubarItem>
-              </MenubarSubContent>
-            </MenubarSub>
-            <MenubarSeparator />
-            <MenubarItem>Cut</MenubarItem>
-            <MenubarItem>Copy</MenubarItem>
-            <MenubarItem>Paste</MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>View</MenubarTrigger>
-          <MenubarContent>
-            <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
-            <MenubarCheckboxItem checked>
-              Always Show Full URLs
-            </MenubarCheckboxItem>
-            <MenubarSeparator />
-            <MenubarItem inset>
-              Reload <MenubarShortcut>⌘R</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem disabled inset>
-              Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
-            </MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem inset>Toggle Fullscreen</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem inset>Hide Sidebar</MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-        <MenubarMenu>
-          <MenubarTrigger>Profiles</MenubarTrigger>
-          <MenubarContent>
-            <MenubarRadioGroup value="benoit">
-              <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
-              <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
-              <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
-            </MenubarRadioGroup>
-            <MenubarSeparator />
-            <MenubarItem inset>Edit...</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem inset>Add Profile...</MenubarItem>
-          </MenubarContent>
+          <MenubarMenu>
+            <MenubarTrigger>Run</MenubarTrigger>
+          </MenubarMenu>
         </MenubarMenu>
       </Menubar>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-    </>
+      <ResizablePanelGroup
+        direction="horizontal"
+        style={{ height: "100%", width: "100%", borderRadius: 10 }}
+      >
+        <ResizablePanel>
+          <Editor
+            height="100%"
+            defaultValue="Write some code here..."
+            theme="vs-dark"
+            value={code}
+            onChange={(c) => setCode(c ?? "")}
+          />
+          {/* <Editor
+            value={code}
+            onValueChange={(code) => setCode(code)}
+            highlight={(code) => code}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 12,
+              height: "100%",
+              width: "100%",
+            }}
+          /> */}
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={50}>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel
+              style={{
+                backgroundColor: "black",
+                display: "flex",
+                flexDirection: "column",
+                gap: 10,
+                padding: 10,
+              }}
+              defaultSize={60}
+            >
+              <Input
+                placeholder="Enter your website.."
+                onChange={(e) => setUrlSrc(e.target.value)}
+                value={urlSrc}
+              />
+              <iframe
+                width="100%"
+                height="100%"
+                style={{ borderRadius: 10 }}
+                src={urlSrc}
+              />
+            </ResizablePanel>
+            <ResizableHandle withHandle />
+            <ResizablePanel
+              defaultSize={40}
+              style={{
+                backgroundColor: "black",
+                padding: 10,
+              }}
+            >
+              <ScrollArea
+                style={{
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 10,
+                  }}
+                >
+                  <Alert>
+                    <AlertTitle>discordSignIn</AlertTitle>
+                    <AlertDescription>
+                      You can add components and dependencies to your app using
+                      the cli.
+                    </AlertDescription>
+                  </Alert>
+                  <Alert>
+                    <AlertTitle>discordSignIn</AlertTitle>
+                    <AlertDescription>
+                      You can add components and dependencies to your app using
+                      the cli.
+                    </AlertDescription>
+                  </Alert>
+                  <Alert>
+                    <AlertTitle>discordSignIn</AlertTitle>
+                    <AlertDescription>
+                      You can add components and dependencies to your app using
+                      the cli.
+                    </AlertDescription>
+                  </Alert>
+                  <Alert>
+                    <AlertTitle>discordSignIn</AlertTitle>
+                    <AlertDescription>
+                      You can add components and dependencies to your app using
+                      the cli.
+                    </AlertDescription>
+                  </Alert>
+                  <Alert>
+                    <AlertTitle>discordSignIn</AlertTitle>
+                    <AlertDescription>
+                      You can add components and dependencies to your app using
+                      the cli.
+                    </AlertDescription>
+                  </Alert>
+                  <Alert>
+                    <AlertTitle>discordSignIn</AlertTitle>
+                    <AlertDescription>
+                      You can add components and dependencies to your app using
+                      the cli.
+                    </AlertDescription>
+                  </Alert>
+                  <Alert>
+                    <AlertTitle>discordSignIn</AlertTitle>
+                    <AlertDescription>
+                      You can add components and dependencies to your app using
+                      the cli.
+                    </AlertDescription>
+                  </Alert>
+                  <Alert>
+                    <AlertTitle>discordSignIn</AlertTitle>
+                    <AlertDescription>
+                      You can add components and dependencies to your app using
+                      the cli.
+                    </AlertDescription>
+                  </Alert>
+                </div>
+              </ScrollArea>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
   );
 }
 
