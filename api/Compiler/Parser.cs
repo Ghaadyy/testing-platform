73ebc512f09abf0a4f7cbd712401e5e4bba7311e
Ghaadyy <ghadyyi@gmail.com>
2024-10-16 14:37:57 +0300

    Removed .dll extension
    To add support for multiple platforms. The runtime will deduce the file.

--- api/Compiler/Parser.cs
+++ api/Compiler/Parser.cs
@@ -4,7 +4,7 @@ namespace RestrictedNL.Compiler;
 
 public static class Parser
 {
-    private const string _dllImportPath = @"librestricted_nl_lib.dll";
+    private const string _dllImportPath = @"librestricted_nl_lib";
 
     [DllImport(_dllImportPath, CallingConvention = CallingConvention.Cdecl)]
     public static extern bool parse(string path);
