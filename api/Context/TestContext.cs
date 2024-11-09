fdaf97381e9e0996df832a791b7a1e5e4f378f4a
Ghaadyy <ghadyyi@gmail.com>
2024-11-09 17:33:36 +0200

    Removed unused Test.cs
    Co-authored-by: Antoine Karam <antoinekaram1414@gmail.com>

--- api/Context/TestContext.cs
+++ api/Context/TestContext.cs
@@ -6,7 +6,6 @@ namespace RestrictedNL.Context;
 public class TestContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<TestFile> TestFiles { get; set; }
-    public DbSet<Test> Tests { get; set; }
     public DbSet<TestRun> TestRuns { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
