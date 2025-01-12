8752847560fc200190daad20ad5308084d73d473
Ghady <69772553+Ghaadyy@users.noreply.github.com>
2025-01-12 20:12:47 +0200

    feat: Added a setup script for SeeClick Flask API (#24)
    Co-authored-by: Antoine Karam <antoinekaram1414@gmail.com>

---
+++ seeclick/api/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install python3 pip git git-lfs nano
+
+git lfs install
+git clone https://huggingface.co/cckevinn/SeeClick
+
+apt install python3.10-venv
+python3 -m venv myenv
+source myenv/bin/activate
+
+pip install -r requirements.txt
+
+python3 app.py
