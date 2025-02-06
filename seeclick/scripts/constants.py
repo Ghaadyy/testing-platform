574ed9aed63c119dcd9997e521d3946183d02502
Ghady <69772553+Ghaadyy@users.noreply.github.com>
2025-02-06 11:04:35 +0200

    feat: Integrate LLAVA model for generating icon descriptions (#47)
    * feat: Integrate LLAVA model for generating icon descriptions
    
    * fix: Merge text and icon element results for the same image
    
    * fix: use pydantic BaseModel for JSON Schema
    
    * fix: Added parse_bbox function + if checks for elements array
    
    ---------
    
    Co-authored-by: A-Karam20 <antoinekaram1414@gmail.com>

--- seeclick/scripts/constants.py
+++ seeclick/scripts/constants.py
@@ -1,41 +1,26 @@
-DEEPSEEK="deepseek-r1-distill-qwen-7b"
+from pydantic import BaseModel
+
+class ImageDescription(BaseModel):
+    description: str
+
+class Element(BaseModel):
+    description: str
+
+class ImageElements(BaseModel):
+    image: str
+    elements: list[Element]
+
+DEEPSEEK = "deepseek-r1-distill-qwen-7b"
+LLAVA = "llava:7b"
 
 OUTPUT_SCHEMA = {
     "type": "json_schema",
     "json_schema": {
-        "schema": {
-            "type": "object",
-            "properties": {
-                "image": {
-                "type": "string"
-                },
-                "elements": {
-                "type": "array",
-                "items": {
-                    "$ref": "#/$defs/element"
-                }
-                }
-            },
-            "$defs": {
-                "element": {
-                "type": "object",
-                "properties": {
-                    "description": {
-                    "type": "string",
-                    "description": "The description of the UI element."
-                    }
-                },
-                "required": ["description"],
-                "additionalProperties": False
-                }
-            },
-            "required": ["image", "elements"],
-            "additionalProperties": False
-        }
-    }
+        "schema": ImageElements.model_json_schema()
+    },
 }
 
-PROMPT = """
+DEEPSEEK_PROMPT = """
 You are given a JSON object containing a list of UI elements from a webpage. Each element contains a "category" (e.g., button, label, checkbox) and optional "text" (e.g., "Submit", "Username"). The "bbox" (bounding box) should be ignored, and no mention of coordinates should appear in your description.
 
 For each element:
@@ -43,4 +28,22 @@ For each element:
 - Describe the element as if you were explaining it to someone, but keep the description brief—no more than a few words or a short sentence.
 - Focus on what the element is and its purpose, based on its category and text, and ignore any bounding box or coordinates.
 - Provide only a concise description as per the output schema and do not mention the bounding box in the description.
+""".strip()
+
+LLAVA_PROMPT = """
+You are given a cropped image of a web UI element, describing an icon button. 
+
+### Your task:
+- For the icon button image, generate **one** brief, human-readable description.
+- Keep the description **concise**.
+- Focus only on **what the icon is**.
+- Do **not** include extra details or explanations.
+- The image is a cropped icon button from a web UI, so only describe the icon itself.
+
+### Example:
+```json
+{
+    "description": "A small refresh icon"
+}
+```
 """.strip()
--- seeclick/scripts/train_data_gen.py
+++ seeclick/scripts/train_data_gen.py
@@ -8,8 +8,12 @@ from tqdm import tqdm
 from ultralytics import YOLO
 from paddleocr import PaddleOCR
 from openai import OpenAI
-from constants import DEEPSEEK, OUTPUT_SCHEMA, PROMPT
+from constants import DEEPSEEK, LLAVA, LLAVA_PROMPT, OUTPUT_SCHEMA, DEEPSEEK_PROMPT
 from dotenv import load_dotenv
+import ollama
+import time
+from constants import ImageDescription
+import torch
 
 load_dotenv()
 
@@ -19,33 +23,112 @@ output_file = os.getenv("OUTPUT_FILE")
 cropped_image_folder = os.getenv("CROPPED_IMG_DIR")
 yolo_images = os.getenv("YOLO_IMG_DIR")
 
-logging.getLogger('ppocr').setLevel(logging.WARNING)
+logging.getLogger("ppocr").setLevel(logging.WARNING)
 
-ocr = PaddleOCR(use_angle_cls=True, lang='en')
+ocr = PaddleOCR(use_angle_cls=True, lang="en")
 
 model = YOLO(YOLO_PATH)
 
-client = OpenAI(
-    base_url="http://localhost:1234/v1",
-    api_key="lm-studio"
-)
+if torch.cuda.is_available():
+    model.to('cuda')
+elif torch.mps.is_available():
+    mode.to('mps')
 
-def get_descriptions(image_result, model=DEEPSEEK):
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+
+
+def get_descriptions_openai(image_result, model=DEEPSEEK):
+    start = time.time()
     completion = client.chat.completions.create(
         model=model,
         messages=[
-            {"role": "system", "content": PROMPT},
-            {"role": "user", "content": json.dumps(image_result)}
+            {"role": "system", "content": DEEPSEEK_PROMPT},
+            {"role": "user", "content": json.dumps(image_result)},
         ],
         temperature=0.7,
-        response_format=OUTPUT_SCHEMA
+        response_format=OUTPUT_SCHEMA,
     )
-    
+    print(f"Time elapsed for DeepSeek R1 {round(time.time() - start, 2)}s")
     return json.loads(completion.choices[0].message.content)
+
+
+def get_descriptions_llava(image_result):
+    icon_descriptions = {"elements": []}
+
+    for element in image_result["elements"]:
+        image_path = element["image_path"]
+        try:
+            start_time = time.time()
+            response = ollama.chat(
+                model=LLAVA,
+                messages=[
+                    {
+                        "role": "user", 
+                        "content": LLAVA_PROMPT, 
+                        "images": [image_path], 
+                        "format": ImageDescription.model_json_schema()
+                    }
+                ],
+            )
+
+            raw_description = response["message"]["content"]
+
+            start = raw_description.find('"description": "') + len('"description": "')
+            end = raw_description.find('"', start)
+            clean_description = raw_description[start:end]
+            icon_descriptions["elements"].append({"description": clean_description})
+            print(f"Time elapsed for LLaVA {round(time.time() - start, 2)}s")
+        except Exception as e:
+            print(f"Error processing image {image_path}: {e}")
+            icon_descriptions["elements"].append({"description": ""})
+    return icon_descriptions
+
+def parse_bbox(boxes):
+    elements = []
+    for box in boxes:
+        x1, y1, x2, y2 = box.xyxy[0]
+
+        class_id = int(box.cls[0])  # Class ID (category index)
+        class_name = result.names[class_id]  # Class name
+
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+        cropped_image = image[y1:y2, x1:x2]
+
+        ocr_result = ocr.ocr(cropped_image, cls=True)
+
+        element = {}
+        if ocr_result[0]:
+            for line in ocr_result[0]:
+                text = line[1][0]  # Extract the text
+                element = {
+                    "bbox": box.xyxy[0].tolist(),
+                    "category": class_name,
+                    "text": text,
+                }
+        else:
+            cropped_image_path = os.path.join(
+                cropped_image_folder,
+                f"{os.path.splitext(img_name)[0]}_cropped_{x1}_{y1}_{x2}_{y2}"
+                f".jpg",
+            )
+            cv2.imwrite(cropped_image_path, cropped_image)
+
+            element = {
+                "bbox": box.xyxy[0].tolist(),
+                "category": class_name,
+                "image_path": cropped_image_path,
+            }
+        
+        elements.append(element)
+
+    return elements
 
 all_results = []
 
-image_files = [img_name for img_name in os.listdir(image_folder) if img_name.endswith(".jpg")]
+image_files = [
+    img_name for img_name in os.listdir(image_folder) if img_name.endswith(".jpg")
+]
 total_images = len(image_files)
 
 for img_name in tqdm(image_files, desc="Processing images", total=total_images):
@@ -53,66 +136,53 @@ for img_name in tqdm(image_files, desc="Processing images", total=total_images):
 
     if not os.path.exists(img_path):
         continue
-    
+
+    start = time.time()
     results = model(img_path)
+    print(f"Time elapsed for YOLOv8 {round(time.time() - start, 2)}s")
     plot = results[0].plot()
 
     image = cv2.imread(img_path)
 
-    image_result = {
-        "image": img_name,
-        "elements": []
-    }
+    image_result = {"image": img_name, "elements": []}
 
     for result in results:
         result.save(filename=f"{yolo_images}{os.path.splitext(img_name)[0]}_yolo.jpg")
 
+        start = time.time()
         boxes = result.boxes  # Get boxes
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
+        boxes = random.sample(list(boxes), 5) if len(boxes) >= 5 else [] # get 5 random boxes
 
-            class_id = int(box.cls[0])  # Class ID (category index)
-            class_name = result.names[class_id]  # Class name
+        image_result["elements"] = parse_bbox(boxes)
+        
+        print(f"Time elapsed for PaddleOCR {round(time.time() - start, 2)}s")
 
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    text_elements = {"image": img_name, "elements": []}
 
-            cropped_image = image[y1:y2, x1:x2]
+    icon_elements = {"image": img_name, "elements": []}
 
-            ocr_result = ocr.ocr(cropped_image, cls=True)
+    for element in image_result["elements"]:
+        if element.get("text"):
+            text_elements["elements"].append(element)
+        else:
+            icon_elements["elements"].append(element)
 
-            element = {}
-            if ocr_result[0]:
-                for line in ocr_result[0]:
-                    text = line[1][0]  # Extract the text
-                    element = {
-                        "bbox": box.xyxy[0].tolist(),
-                        "category": class_name,
-                        "text": text
-                    }
-            else:
-                cropped_image_path = os.path.join(cropped_image_folder,
-                                                    f"{os.path.splitext(img_name)[0]}_cropped_{x1}_{y1}_{x2}_{y2}"
-                                                    f".jpg")
-                cv2.imwrite(cropped_image_path, cropped_image)
+    text_descriptions = get_descriptions_openai(text_elements)
+    if len(text_elements["elements"]) == len(text_descriptions["elements"]):
+        for i, desc in enumerate(text_descriptions["elements"]):
+            text_elements["elements"][i]["description"] = desc["description"]
 
-                element = {
-                    "bbox": box.xyxy[0].tolist(),
-                    "category": class_name,
-                    "text": "No text detected"
-                }
+    icon_descriptions = get_descriptions_llava(icon_elements)
+    if len(icon_elements["elements"]) == len(icon_descriptions["elements"]):
+        for i, desc in enumerate(icon_descriptions["elements"]):
+            icon_elements["elements"][i]["description"] = desc["description"]
 
-            image_result["elements"].append(element)
-            
-    image_result["elements"] = random.sample(image_result["elements"], 5)
+    merged_elements = {
+        "image": img_name,
+        "elements": text_elements["elements"] + icon_elements["elements"],
+    }
 
-    descriptions = get_descriptions(image_result)
+    all_results.append(merged_elements)
 
-    print(descriptions)
-    
-    for i, desc in enumerate(descriptions['elements']):
-        image_result['elements'][i]['description'] = desc['description']
-
-    all_results.append(image_result)
-
-with open(output_file, 'w') as outfile:
+with open(output_file, "w") as outfile:
     json.dump(all_results, outfile, indent=4)
