dfc6506979a7f79000bd9473e2f49781cba35e64
Ghaadyy <ghadyyi@gmail.com>
2025-04-02 18:03:48 +0300

    ci: Used production version of client + nginx in Dockerfile

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import "@/index.css";
 import App from "@/App";
 import AppProvider from "@/context/AppProvider";
 
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = window.env?.VITE_API_URL || import.meta.env.VITE_API_URL;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
