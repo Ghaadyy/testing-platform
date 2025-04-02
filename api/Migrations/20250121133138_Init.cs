c328712b09c6fb2c30e78830771470bdf50760d6
Ghaadyy <ghadyyi@gmail.com>
2025-04-02 20:51:53 +0300

    api: Add Migrations to git

---
+++ api/Migrations/20250121133138_Init.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestingPlatform.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "assertions",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    run_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    test_name = table.Column<string>(type: "text", nullable: false),
+                    message = table.Column<string>(type: "text", nullable: false),
+                    passed = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_assertions", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "log_groups",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    test_name = table.Column<string>(type: "text", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    run_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_log_groups", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "test_files",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_test_files", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "test_runs",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    file_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ran_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    compiled_code = table.Column<string>(type: "text", nullable: false),
+                    raw_code = table.Column<string>(type: "text", nullable: false),
+                    duration = table.Column<long>(type: "bigint", nullable: false),
+                    passed = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_test_runs", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    user_name = table.Column<string>(type: "text", nullable: false),
+                    dob = table.Column<DateOnly>(type: "date", nullable: true),
+                    password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "assertions");
+
+            migrationBuilder.DropTable(
+                name: "log_groups");
+
+            migrationBuilder.DropTable(
+                name: "test_files");
+
+            migrationBuilder.DropTable(
+                name: "test_runs");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
---
+++ api/Migrations/20250125170513_EnhanceRun.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestingPlatform.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnhanceRun : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "passed",
+                table: "test_runs");
+
+            migrationBuilder.AddColumn<int>(
+                name: "status",
+                table: "test_runs",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "test_runs");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "passed",
+                table: "test_runs",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
